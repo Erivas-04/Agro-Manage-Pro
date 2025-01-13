dc24245153ded78d7d6bc8652e4a18104ab40a40
Erivas-04 <ericrivas192@gmail.com>
2025-01-13 16:08:38 -0600

    correcciones de front

--- agromanagepro-1/src/app/demo/components/modules/configurations/configurations.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-configurations',
-  templateUrl: './configurations.component.html'
-})
-export class ConfigurationsComponent {
-
-}
--- agromanagepro-1/src/app/demo/components/modules/move/move.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-move',
-  templateUrl: './move.component.html'
-})
-export class MoveComponent {
-
-}
